8d78560bf2d7f5defcb2d553be44b3e71fbbaf98
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-19 02:54:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function deleteFromObjectByKey(object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete object.key
-  return object
+  return delete object.key
 }
 
 
