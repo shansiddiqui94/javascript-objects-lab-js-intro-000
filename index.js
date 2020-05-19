dbc460a77d2d021d45afd597595550006d2e9a16
Shan Siddiqui <shansiddiqui94@gmail.com>
2020-05-19 02:21:42 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function updateObjectWithKeyAndValue(object, key, value){
+var object = Object.assign({}, {[key]: value})
+     //obj.prop2 = 'undefined'
+    // obj['prop2'] = 'undefined';
+  return object
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object
+  }
+
+function deleteFromObjectByKey(object, key){
+  var obj = Object.assign({prop: 1})
+  var obj2 = delete obj.key
+  return obj2
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  var obj = {prop: 1 }
+  delete obj.prop
+  return obj
+}
